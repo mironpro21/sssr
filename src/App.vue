4ba583c970a4485afdd3e5eadcb7ef27ebf5755b
SSSR <kuku4251g@gmail.com>
2024-11-08 14:14:00 +0500

    123123

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div class="bg-black m-11 text-red-800">123</div>
+  <div class="bg-black m-11 text-red-800">
+    123
+  </div>
 
 </template>
 
